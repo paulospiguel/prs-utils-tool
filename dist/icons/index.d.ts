c4da89e1d902c2f994370ae8f4a45d6d9d73111d
Paulo Spiguel <paulospil@hotmail.com>
2023-07-20 16:06:24 +0100

    add icons

---
+++ dist/icons/index.d.ts
@@ -0,0 +1,4 @@
+export { ReactComponent as Maestro } from "./assets/flat/maestro.svg";
+export { ReactComponent as Mastercard } from "./assets/flat/mastercard.svg";
+export { ReactComponent as Paypal } from "./assets/flat/paypal.svg";
+export { ReactComponent as Visa } from "./assets/flat/visa.svg";
